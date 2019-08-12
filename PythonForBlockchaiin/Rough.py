002f8b761e12817c2f0d2269af464f8972ef2e9a
unknown <olugbadeoluwatobi@yahoo.com>
2019-08-12 23:35:34 +0100

    Re-add roughwork file as rough.py

---
+++ PythonForBlockchaiin/Rough.py
@@ -0,0 +1,12 @@
+#This file is for testing personal scripts
+
+
+def personal_profiler():
+    first_Name= (input("Please enter your First Name: "))
+    last_name= (input("Please enter your last name: "))
+    sex= (input("Please enter your gender: "))
+    age_range= (input("Please enter your age: "))
+    print(first_Name)
+
+
+personal_profiler()
