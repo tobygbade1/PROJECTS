dfd611c03568c8864e090b8e79dc380e307f5bec
unknown <olugbadeoluwatobi@yahoo.com>
2019-08-14 05:36:20 +0100

    Included function to verify chain in program

--- PythonForBlockchaiin/Blockchain.py
+++ PythonForBlockchaiin/Blockchain.py
@@ -31,13 +31,35 @@ def print_blockchain_elements():
     # Outputs the blockchain list to the console
     for block in blockchain:
         print("Outputting block")
-        print(block)1
+        print(block)
+    else:
+        print('-' * 20)
 
 
-while True:
+def verify_chain():
+    block_index = 0
+    is_valid = True
+    for block in blockchain:
+        if block_index == 0:
+            block_index += 1
+            continue
+        elif block[0] == blockchain[block_index-1]:
+            is_valid = True
+        else:
+            is_valid = False
+            break
+        block_index += 1
+    return is_valid
+
+
+waiting_for_input = True
+
+
+while waiting_for_input:
     print("Please choose: ")
     print("1: Add a new transaction value")
     print("2: Output the blockchain blocks")
+    print("h: Manipulate the chain")
     print("q: Quit")
     user_choice = get_user_choice()
     if user_choice == '1':
@@ -45,11 +67,19 @@ while True:
         add_transaction(tx_amount, get_last_blockchain_value())
     elif user_choice == '2':
         print_blockchain_elements()
+    elif user_choice == 'h':
+        if len(blockchain) >= 1:
+            blockchain[0] = [2]
     elif user_choice == 'q':
         print("You have chosen to exit the program")
-        break
+        waiting_for_input = False
     else:
         print("Input was invalid, please pick a value from the list!")
+    if not verify_chain():
+        print("Invalid blockchain!")
+        break
+else:
+    print("User left")
 
 
 print("Done!")
